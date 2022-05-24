f0b51a1dca71695d5d652049ef296985e3a1b6fb
Chanchal Pradhan <94612450+chanchaltut@users.noreply.github.com>
2022-05-24 22:28:30 +0530

    version 1.0

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+// const navToggle =
+//   document.querySelector('.nav-toggle');
+
+// navToggle.addEventListener('click', () => {
+//   document.body.classList.toggle('nav-open');
+// });
+
+let big_wrapper;
+let hamburger_menu;
+
+function declare() {
+    big_wrapper = document.querySelector(".big-wrapper");
+    hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+
+declare();
+
+function events() {
+    hamburger_menu.addEventListener("click", () => {
+        big_wrapper.classList.toggle("nav-open");
+    });
+}
+
+events();
+
+
+const links = document.querySelectorAll(".my-nav-link");
+
+function closeMenu() {
+    console.log("sameer");
+    big_wrapper.classList.remove("nav-open");
+    console.log("chanchal");
+}
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
 const myCarouselElement = document.querySelector('#myCarousel')
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 5000,
-  wrap: false
+    interval: 5000,
+    wrap: false
 })
